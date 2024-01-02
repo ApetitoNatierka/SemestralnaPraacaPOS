23a4e629d9a8f8a764d56f57f5c24b2e92a324ed
ApetitoNatierka <keresfilip@gmail.com>
2024-01-02 17:29:40 +0100

    zaciatok

---
+++ cmake-build-debug/CMakeFiles/semestralna_praca_pos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/semestralna_praca_pos.dir/main.cpp.o"
+  "semestralna_praca_pos"
+  "semestralna_praca_pos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/semestralna_praca_pos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
